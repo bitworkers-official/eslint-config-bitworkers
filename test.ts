beb95efaa43d89a56f9ab98228e4bf373f8b0a7e
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-01-26 19:23:54 +0100

    feat: comment out something

--- test.ts
+++ test.ts
@@ -3,10 +3,10 @@ const d = a => 1 + a
 d(() => 1)
 
 // eslint-disable-next-line use-isnan
-if    (NaN === 1) {
+if (NaN === 1) {
 }
 
-const [, , x] = [0, 0, 1]
+// const [, , x] = [0, 0, 1]
 console.log(x)
 
 // eslint-disable-next-line import/prefer-default-export
