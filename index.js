85d9a66e2e770e36fe556dd86ab3a50cfca17395
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-01-26 19:38:44 +0100

    fix: remove airbnb because its not working with typescript

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    'airbnb-base',
+    // 'airbnb-base',
     'plugin:unicorn/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
